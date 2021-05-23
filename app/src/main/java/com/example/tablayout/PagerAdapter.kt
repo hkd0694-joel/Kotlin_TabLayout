c947699f4d73de07ff0f3be78fe6c40029c8bb2c
한경동 <joel@youfriend.kr>
2021-05-23 13:45:24 +0900

    FragmentStatePagerAdapter 정의

---
+++ app/src/main/java/com/example/tablayout/PagerAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.tablayout
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import java.util.*
+
+/**
+ * Tablayout
+ * Class: PagerAdapter
+ * Created by 한경동 (Joel) on 2021/05/23.
+ * Description:
+ */
+class PagerAdapter(fm: FragmentManager): FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    private var fragmentList: MutableList<SampleFragment> = ArrayList()
+
+    override fun getCount(): Int = fragmentList.size
+
+    override fun getItem(position: Int): Fragment = fragmentList[position]
+
+    fun addItems(fragment: SampleFragment) = fragmentList.add(fragment)
+
+}
